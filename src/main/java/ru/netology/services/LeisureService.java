62480a013b8ed59e0a91e1fbbc4591e45594cbd2
ZloyMatvey <122729209+ZloyMatvey@users.noreply.github.com>
2023-05-23 00:24:23 +0300

    Update commit

--- src/main/java/ru/netology/services/LeisureService.java
+++ src/main/java/ru/netology/services/LeisureService.java
@@ -7,7 +7,8 @@ public class LeisureService {
         for (int month = 0; month < 12; month++) {
             if (money > threshold) {
                 count++;
-                money = (money - expenses) - (money / 3 * 2);
+                int remainder = money - expenses;
+                money = money - expenses - (remainder / 3 * 2);
             } else {
                 money = money + income - expenses;
             }
